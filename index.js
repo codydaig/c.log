602906ba4fa0106a153981831c543151abd69d42
Cody B. Daig <cody@daig.me>
2015-09-21 19:42:10 -0700

    [cleanup] Cleanup Code

--- index.js
+++ index.js
@@ -2,39 +2,39 @@
 
 var chalk = require('chalk');
 
-var clog = function(value) {
+var clog = function (value) {
   console.log(value);
 };
 
-clog.green = function(value) {
+clog.green = function (value) {
   console.log(chalk.green(value));
 };
 
-clog.blue = function(value) {
+clog.blue = function (value) {
   console.log(chalk.blue(value));
 };
 
-clog.red = function(value) {
+clog.red = function (value) {
   console.log(chalk.red(value));
 };
 
-clog.yellow = function(value) {
+clog.yellow = function (value) {
   console.log(chalk.yellow(value));
 };
 
-clog.magenta = function(value) {
+clog.magenta = function (value) {
   console.log(chalk.magenta(value));
 };
 
-clog.cyan = function(value) {
+clog.cyan = function( value) {
   console.log(chalk.cyan(value));
 };
 
-clog.white = function(value) {
+clog.white = function (value) {
   console.log(chalk.white(value));
 };
 
-clog.gray = function(value) {
+clog.gray = function (value) {
   console.log(chalk.gray(value));
 };
 
